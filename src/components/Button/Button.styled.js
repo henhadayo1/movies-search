aefc46a70fb0fb9b269f54ca6a5a98c64013969a
henhadayo1 <henhadayo@gmail.com>
2023-11-14 19:03:07 +0200

    Styling button

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,3 +1,16 @@
 import styled from "styled-components";
 
-export const StyledButton = styled.button``;
+export const StyledButton = styled.button`
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #4472c4;
+  color: white;
+  cursor: pointer;
+  transition: border-color 0.25s;
+  &:hover {
+    border-color: #31538f;
+  }
+`;
