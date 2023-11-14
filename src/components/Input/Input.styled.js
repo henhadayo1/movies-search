06f8d9b40aece3dbfb5971cdd1755ed5349c8b2f
henhadayo1 <henhadayo@gmail.com>
2023-11-14 19:10:32 +0200

    Style Input

--- src/components/Input/Input.styled.js
+++ src/components/Input/Input.styled.js
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const StyledInput = styled.input``;
+export const StyledInput = styled.input`
+  border: 1px solid #31538f;
+  outline: none;
+  padding: 0.8em 1.4em;
+  font-size: 1em;
+  font-weight: 400;
+  font-family: inherit;
+  transition: border-color 0.25s;
+`;
