d640bf51626e20d9ddb39863bcb689ee6cc13a8a
henhadayo1 <henhadayo@gmail.com>
2023-11-14 18:34:35 +0200

    Add movies api url into a .env file and use it accordingly

--- src/services/movies-service.js
+++ src/services/movies-service.js
@@ -3,15 +3,12 @@ import axios from "axios";
 export async function getMoviesByName(name, page = 1) {
   let result = { data: {}, error: "" };
   try {
-    const response = await axios.get(
-      "https://jsonmock.hackerrank.com/api/movies/search",
-      {
-        params: {
-          Title: name,
-          page,
-        },
-      }
-    );
+    const response = await axios.get(import.meta.env.VITE_MOVIES_API_URL, {
+      params: {
+        Title: name,
+        page,
+      },
+    });
     result.data = response.data;
   } catch (error) {
     result.error = error.message;
