c87f19a16ee2e8cedc969d390115f5c2bda52986
henhadayo1 <henhadayo@gmail.com>
2023-11-16 11:53:18 +0200

    Add sorting functionality

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import Pagination from "./Pagination";
 import { StyledTable } from "./Table.styled";
 import { StyleTableWrapper } from "./TableWrapper.styled";
+import { SORTING_ORDER, sortArrayOfObjectsByKey } from "../utils/utils";
 
 export const Table = ({
   data,
@@ -10,20 +12,44 @@ export const Table = ({
   currentPage,
   onPageClick,
 }) => {
+  const [rowsData, setRowsData] = useState(data);
+  const [columnToSort, setColumnToSort] = useState(undefined);
+  const [sortingOrder, setSortingOrder] = useState(undefined);
+
+  useEffect(() => {
+    setRowsData(data);
+  }, [data]);
+
+  function headClickEventHandler(columnName) {
+    let _sortingOrder =
+      sortingOrder === SORTING_ORDER.DESC || columnName !== columnToSort //columnToSort is the previous column to sort, before I set this state
+        ? SORTING_ORDER.ASC
+        : SORTING_ORDER.DESC;
+
+    sortArrayOfObjectsByKey(rowsData, columnName, _sortingOrder);
+    setRowsData(rowsData);
+    setSortingOrder(_sortingOrder);
+    setColumnToSort(columnName);
+  }
+
   return (
     <StyleTableWrapper>
       <StyledTable>
         <thead>
           <tr>
             {columns.map((column) => (
-              <th key={column.title} className="clickable">
+              <th
+                key={column.title}
+                className="clickable"
+                onClick={() => headClickEventHandler(column.name)}
+              >
                 {column.title}
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {data.map((movie) => (
+          {rowsData.map((movie) => (
             <tr key={movie[keyProp]}>
               {columns.map((column) => (
                 <td key={column.title}>{movie[column.name]}</td>
--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,7 +1,35 @@
+export const SORTING_ORDER = {
+  ASC: "asc",
+  DESC: "desc",
+};
+
 export function arrayFromNumber(num) {
   let arr = [];
   for (let i = 1; i <= num; i++) {
     arr.push(i);
   }
   return arr;
+}
+
+export function sortArrayOfObjectsByKey(arr, key, order = SORTING_ORDER.ASC) {
+  if (order === SORTING_ORDER.ASC) {
+    return arr.sort((a, b) => {
+      if (a[key] < b[key]) {
+        return -1;
+      }
+      if (a[key] > b[key]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  return arr.sort((a, b) => {
+    if (a[key] < b[key]) {
+      return 1;
+    }
+    if (a[key] > b[key]) {
+      return -1;
+    }
+    return 0;
+  });
 }
